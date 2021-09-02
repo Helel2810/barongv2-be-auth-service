ce3c47ec61976cbf976f59e49ca377c17e8ee6fd
Arief Rufaidy <webdev2@mg.co.id>
2021-09-02 13:48:12 +0700

    Initial commit

---
+++ app/api/models.py
@@ -0,0 +1,19 @@
+from typing import List
+from pydantic import BaseModel
+
+SECRET_KEY = 'minimal-secret-key'
+JWT_ALGORITHM = 'HS256'
+ACCESS_TOKEN_EXPIRES_MINUTES = 10
+
+
+class User(BaseModel):
+    email: str
+    password: str
+
+class Token(BaseModel):
+    access_token: str
+
+class JWTSettings(BaseModel):
+    authjwt_secret_key: str = SECRET_KEY
+    authjwt_algorithm: str = JWT_ALGORITHM
+    authjwt_access_token_expires: int = ACCESS_TOKEN_EXPIRES_MINUTES
---
+++ app/api/users.py
@@ -0,0 +1,35 @@
+from typing import List
+from fastapi import Header, APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi_jwt_auth import AuthJWT
+
+from app.api.models import User, Token, JWTSettings
+
+
+@AuthJWT.load_config
+def load_config():
+    return JWTSettings()
+
+users = APIRouter()
+
+def authenticate_user(username: str, password: str):
+    if username != 'demo@minimals.cc':
+        return False
+    if password != 'demo1234':
+        return False
+
+    return {'id': '8864c717-587d-472a-929a-8e5f298024da-0'}
+
+
+@users.post('/token', response_model=Token)
+async def login_for_access_token(form_data: User, Authorize: AuthJWT = Depends()):
+    user = authenticate_user(form_data.email, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code= status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect Username or Passwords"
+        )
+    access_token = Authorize.create_access_token(subject=user['id'])
+    return {'access_token' : access_token}
+
+
